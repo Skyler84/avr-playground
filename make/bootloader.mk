97d2ee716024e076e3d35844218958cd0a259ce9
Your Name <you@example.com>
2025-04-22 10:20:52 +0100

    adding functionality to makefiles

--- make/bootloader.mk
+++ make/bootloader.mk
@@ -5,22 +5,39 @@
 #   LOAD_ADDR -> hexadecimal address to load bootloader
 
 
-ifndef LOAD_ADDR
-$(error LOAD_ADDR not defined for module target ${TARGET} in ${DIR})
+ifndef BOOT_START_ADDR
+$(error BOOT_START_ADDR not defined for module target ${TARGET} in ${DIR})
 endif
 
 ${DIR}_BOOT_DISCARDS := ${patsubst %,${BLD_DIR}/${DIR}/${TARGET}.boot.%, elf hex bin} ${SRCS:%.c=${BLD_DIR}/${DIR}/%.boot.o}
 ${DIR}_BOOT_CSRC := ${SRCS:%.c=${DIR}/%.c}
 ${DIR}_BOOT_OBJS := ${SRCS:%.c=${BLD_DIR}/${DIR}/%.boot.o}
-${DIR}/${TARGET}_BOOT_LOAD_ADDR := ${LOAD_ADDR}
+${DIR}_BOOT_START_ADDR := ${BOOT_START_ADDR}
+${DIR}_BOOT_MODULE_START_ADDR := ${MODULE_START_ADDR}
 
 DIR_FROM_BOOT_OBJ = ${patsubst ${BLD_DIR}/%/$*.boot.o,%,$@}
 ${DIR}_BOOT_CFLAGS :=  ${CFLAGS} ${${DIR}_CFLAGS}
-${DIR}_BOOT_LDFLAGS :=  ${LDFLAGS} ${${DIR}_LDFLAGS} -Wl,-section-start=.text=${LOAD_ADDR}
+${DIR}_BOOT_LDFLAGS :=  ${LDFLAGS} ${${DIR}_LDFLAGS} -Wl,-section-start=.text=${BOOT_START_ADDR}
+${DIR}_BOOT_MODULES := ${MODULES}
 
 include ${MAKEDIR}/internal/targets.mk
 
 ${BLD_DIR}/${DIR}/all:: ${patsubst %,${BLD_DIR}/${DIR}/${TARGET}.boot.%, elf hex bin}
+
+${BLD_DIR}/${DIR}/%.boot.mod.bin : ${BLD_DIR}/modules/%.mod.bin
+	@echo $@
+	mkdir -p ${@D}
+	cp $< $@
+
+${BLD_DIR}/${DIR}/${TARGET}.boot.mods.bin : ${BLD_DIR}/%/${TARGET}.boot.mods.bin : ${patsubst %,${BLD_DIR}/${DIR}/%.boot.mod.bin,${${DIR}_BOOT_MODULES}}
+	mkdir -p ${@D}
+	srec_cat ${patsubst %,% -binary,$^} -o $@ -binary
+
+${BLD_DIR}/${DIR}/${TARGET}.boot.mods.hex : ${BLD_DIR}/%/${TARGET}.boot.mods.hex : ${BLD_DIR}/%/${TARGET}.boot.mods.bin
+	${OBJCOPY} -O ihex -I binary $< $@ --change-addresses ${$*_BOOT_MODULE_START_ADDR}
+
+${BLD_DIR}/${DIR}/${TARGET}.boot.all.hex : ${BLD_DIR}/%/${TARGET}.boot.all.hex : ${BLD_DIR}/%/${TARGET}.boot.mods.hex ${BLD_DIR}/%/${TARGET}.boot.hex
+	srec_cat $< -intel -o $@ -intel
 
 ${BLD_DIR}/${DIR}/${TARGET}.boot.bin : ${BLD_DIR}/%.boot.bin : ${BLD_DIR}/${DIR}/${TARGET}.boot.elf
 	${OBJCOPY} -O binary $< $@
@@ -39,4 +56,4 @@ ${${DIR}_BOOT_OBJS} : ${BLD_DIR}/${DIR}/%.boot.o : ${DIR}/%.c
 ${BLD_DIR}/${DIR}/clean::
 	rm -f ${${patsubst ${BLD_DIR}/%/clean,%,$@}_BOOT_DISCARDS}
 
-undefine LOAD_ADDR
+undefine BOOT_START_ADDR
